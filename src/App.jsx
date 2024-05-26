55a27b577bddba63ccdbe235edc4878a6ba08989
PRakhar <srivastavaprakhar3010@gmail.com>
2024-05-26 16:01:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <h1>ToDo_List</h1>
+      <p>
+        <ul>
+          <li>"new"-Add a Todo</li>
+          <li>"list"-List all Todos</li>
+          <li>"delete"-Remove a specific Todo</li>
+          <li>"quit"-close the app</li>
+        </ul>
+      </p>
+    </>
+  );
+}
+
+export default App;
